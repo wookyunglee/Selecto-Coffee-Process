c6fbfa2bd0e11af543dbeb10d923409eb059e10e
Wookyung <saco960@gmail.com>
2023-06-01 10:29:34 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// $(document).ready(function(){}) == $(function(){})
+// // 	충돌을 막기 위해서 html이 끝나고 js 시작
+// // document:html이 무거울 경우 사용
+// // 함수가 제대로 작동하는 지 확인하는 방법 2가지
+// 1. alert("text")
+// $(document).redy(function(){
+// 	$("class name").click(function(){
+// 		 alert("알림");
+// 	})
+// });
+
+
+// console.clear();
+// // 콘솔 창 깨끗하게 지우기 : 문서 시작 할때 가장 위에 씀
+
+// 2.console.log("text")
+// $(document).ready(function(){
+// 	$("class name").click(function(){
+//     console.log("콘솔")
+// 	})
+// });
+
+
+// //     let = var : 정의를 내린다.
